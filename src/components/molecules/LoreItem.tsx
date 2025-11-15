728e00667951b28d1295c7c6f085dff3e5e37b41
Raymond Cox <raymondc1911@gmail.com>
2025-11-14 19:10:37 -0500

    Refactor LoreItem component to improve loading state handling by integrating Skeleton directly within the Card component.

--- src/components/molecules/LoreItem.tsx
+++ src/components/molecules/LoreItem.tsx
@@ -45,34 +45,34 @@ export default function LoreItem({ isLoaded, _id }: LoreItemProps) {
     },
   ]
 
-  return (
-    <Skeleton loading={showSkeleton} active>
-      <Card
-        style={{ width: 350, minHeight: 300 }}
-        title={
-          <Flex justify="space-between" align="center" gap={8}>
-            <Title
-              level={4}
-              style={{
-                margin: 0,
-                flex: 1,
-                whiteSpace: 'normal',
-                padding: '10px 0',
-              }}
-            >
-              {title}
-            </Title>
-            <LoreMenu _id={_id} />
-          </Flex>
-        }
-      >
-        <Flex vertical justify="space-between" style={{ minHeight: 200 }}>
-          <Text style={{ marginBottom: 20, whiteSpace: 'pre-wrap' }}>
-            {text}
-          </Text>
-          <Collapse items={items} ghost />
+  return showSkeleton ? (
+    <Card style={{ width: 350, minHeight: 300 }}>
+      <Skeleton active paragraph={{ rows: 6 }} />
+    </Card>
+  ) : (
+    <Card
+      style={{ width: 350, minHeight: 300 }}
+      title={
+        <Flex justify="space-between" align="center" gap={8}>
+          <Title
+            level={4}
+            style={{
+              margin: 0,
+              flex: 1,
+              whiteSpace: 'normal',
+              padding: '10px 0',
+            }}
+          >
+            {title}
+          </Title>
+          <LoreMenu _id={_id} />
         </Flex>
-      </Card>
-    </Skeleton>
+      }
+    >
+      <Flex vertical justify="space-between" style={{ minHeight: 200 }}>
+        <Text style={{ marginBottom: 20, whiteSpace: 'pre-wrap' }}>{text}</Text>
+        <Collapse items={items} ghost />
+      </Flex>
+    </Card>
   )
 }
